7abbaa40bd1c581c87140c657fb29238ebc8e8f7
KennethWang1 <122188744+KennethWang1@users.noreply.github.com>
2026-01-29 03:39:19 -0500

    less hot?

--- runner.py
+++ runner.py
@@ -133,7 +133,7 @@ def training_cycle():
         if cycle_count % 5 == 0:
             time.sleep(300)
         else:
-            time.sleep(20)
+            time.sleep(45)
     
     # Final cleanup
     with model_lock:
